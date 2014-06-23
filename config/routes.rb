a1132b935edef8b0be6862b9722b1416a6424c6d
Lucas Roman <lucasroman@gmail.com>
2014-06-23 20:02:43 -0300

    Agregada la ruta para welcome.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'welcome/page'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'posts#index'
+   root 'welcome#page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
