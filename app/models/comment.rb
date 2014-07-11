3aa43cb6214a373157d858d17b09f068b24d5280
Lucas Roman <lucasroman@gmail.com>
2014-07-11 11:20:33 -0300

    Código comentado.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+	belongs_to :post
+
+=begin
+  def post
+  	Post.find(post_id)
+  end 
+
+  def comments
+  	Comment.where({post_id: id})
+  end
+
+  def ultimo
+  	Comment.last
+  end
+=end
+
 end
