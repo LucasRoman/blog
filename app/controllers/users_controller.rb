7d9964ec5449b9f1d75bbe5fff1aea6c3e25387d
Lucas Roman <lucasroman@gmail.com>
2014-07-11 11:58:31 -0300

    Agregadas acciones index, new y create a UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
+    @users = User.all
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:email])
+    if @user.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color] = "valid"
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color] = "invalid"
+    end
+    render "new"
+    
 end
