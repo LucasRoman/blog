e0ff36f83dd6861f5f0049c6001270a1c7e26d09
Lucas Roman <lucasroman@gmail.com>
2014-07-15 18:56:22 -0300

    Agregados métodos para crear y borrar sesión.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
-
+  	#session[:user_id] = @user.id
   end
 
   def create
+  	if @user = User.authenticate(params[:user])
     session[:user_id] = @user.id
+    redirect_to root_url
+	end
   end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to root_url
+  end
+
 end
