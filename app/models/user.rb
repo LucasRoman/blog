12bd89ddb7672c6281bebbc5430989d20e7e9584
Lucas Roman <lucasroman@gmail.com>
2014-07-15 10:45:24 -0300

    Agregado código para encriptación y validación de datos.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ActiveRecord::Base
 
-	before_action :authenticate_user!
+  attr_accessor :password, :password_confirmation
+
+  validate :validation_password
   
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  before_create :encrypt
+
+  def validation_password!
+    errors.add(:password, 'Passwords must be equal.') unless password == password_confirmation
+  end
+
+  def encrypt
+    passwors_hash = Digest::MD5.digest(password)
+  end
+
+  def self.authenticate_user(user_id)
+    User.find(:user_id)
+  end
+
+
 end
