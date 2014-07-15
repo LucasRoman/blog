303ddded3f0d582985718f6760e04ec9c3529230
Lucas Roman <lucasroman@gmail.com>
2014-07-15 14:37:27 -0300

    Agregada acción update de user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,24 @@ class UsersController < ApplicationController
     if @user.save
       flash[:notice] = "You signed up successfully"
       flash[:color] = "valid"
+      redirect_to :back
     else
       flash[:notice] = "Form is invalid"
       flash[:color] = "invalid"
     end
-    render "new"
+    render 'new'
   end
   
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
-  
+
   private
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
