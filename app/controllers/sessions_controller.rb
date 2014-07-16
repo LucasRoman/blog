d0a1cc777684dc363cdc99e8c63e493e3cf6cee8
Lucas Roman <lucasroman@gmail.com>
2014-07-16 10:23:51 -0300

    Agregadas verificaciones para crear sesión.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,23 @@
 class SessionsController < ApplicationController
 
   def new
-  	#session[:user_id] = @user.id
   end
 
   def create
-  	if @user = User.authenticate(params[:user])
-    session[:user_id] = @user.id
-    redirect_to root_url
-	end
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:user_id])
+   	  session[:user_id] = user.id
+      sign_in user
+      redirect_to user
+    else
+      flash[:notice] = 'User not found.'
+      redirect_to 'new'
+    end
   end
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to root_url
+  	redirect_to 'new'
   end
 
 end
