d81447003b0de6da28cca204b73758156cb2da8a
Lucas Roman <lucasroman@gmail.com>
2014-07-16 10:44:27 -0300

    Agregada lógica para autenticar usuario.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,16 @@ class User < ActiveRecord::Base
     passwors_hash = Digest::MD5.digest(password)
   end
 
-  def self.authenticate_user(user_id)
+  def self.authenticate_user(:user_id)
     User.find(:user_id)
+    if user
+      session[:user_id] = user.id
+      sign_in user
+      redirect_to user
+    else
+      flash[:notice] = 'User not found.'
+      redirect_to 'new'
+    end
   end
 
 end
