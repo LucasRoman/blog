d972870027e090269698b2f6610267ef8cb8f663
Lucas Roman <lucasroman@gmail.com>
2014-07-16 10:43:45 -0300

    Transferida lógica de create de sessions al modelo de user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:user_id])
-   	  session[:user_id] = user.id
-      sign_in user
-      redirect_to user
-    else
-      flash[:notice] = 'User not found.'
-      redirect_to 'new'
-    end
+    User.authenticate_user(params[:user_id])
   end
 
   def destroy
