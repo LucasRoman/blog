e6e16035efc12ea8ee74bc6367dd9b3cf9563078
Lucas Roman <lucasroman@gmail.com>
2014-07-19 11:58:19 -0300

    Eliminado private innecesario.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,9 @@ class PostsController < ApplicationController
   http_basic_authenticate_with name: "Lucas", password: "123", except: [:index, :show]
   before_action :authenticate_user!
  
-
   def klass
     Post
   end
 
-  private
- 
 
 end
