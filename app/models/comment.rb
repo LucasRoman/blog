d192fb9b7fb1d185127a7638f7dd50f90298586f
Lucas Roman <lucasroman@gmail.com>
2014-07-19 11:58:44 -0300

    Eliminado código comentado y agregado un scope.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,18 +1,6 @@
 class Comment < ActiveRecord::Base
 	belongs_to :post
 
-=begin
-  def post
-  	Post.find(post_id)
-  end 
-
-  def comments
-  	Comment.where({post_id: id})
-  end
-
-  def ultimo
-  	Comment.last
-  end
-=end
+  scope :params, -> { find params[:id]  }
 
 end
