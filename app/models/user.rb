665e369c4e1c7bc96c9270218e34b681faba05fb
Lucas Roman <lucasroman@gmail.com>
2014-07-19 10:37:38 -0300

    Cambidada la autenticación en modelo user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,20 @@
 class User < ActiveRecord::Base
 
+  attr_accessor :password, :password_confirmation
+  
+  before_create :encrypt
+  
   validates :email, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
-  
-  before_create :encrypt
 
-  attr_accessor :password, :password_confirmation
-
-  def encrypt
-    passwors_hash = Digest::MD5.digest(password)
+  def self.authenticate_user email, password
+    binding.pry
+    find_by email: email, password_hash: Digest::MD5.digest(password).force_encoding('utf-8') 
   end
 
-  def self.authenticate_user(:user_id)
-    User.find(:user_id)
-    if user
-      session[:user_id] = user.id
-      sign_in user
-      redirect_to user
-    else
-      flash[:notice] = 'User not found.'
-      redirect_to 'new'
-    end
+  def encrypt
+    self.password_hash = Digest::MD5.digest(password).force_encoding('utf-8')
   end
 
 end
